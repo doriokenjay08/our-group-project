a2c74ed2b876bec9199d6b9b0e953eb54231c9f0
cyyy <drincelannsalapang@gmail.com>
2026-01-30 00:58:48 +0800

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package multiplication;
+
+/**
+ *
+ * @author drinc
+ */
+public class Multiplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        case 3:
+            System.out.println(“Result: “ + (a * b);
+            
+        break; 
+
+        default: 
+            System.out.println(“Exiting…”);
+            
+
+    }
+    
+}
